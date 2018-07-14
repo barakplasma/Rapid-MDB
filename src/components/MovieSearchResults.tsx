ee0be5b0d5174a5041ca3647e0b52561f6e4faf1
Michael Salaverry <barakplasma@gmail.com>
2018-07-14 16:38:52 +0300

    added image place holder

--- src/components/MovieSearchResults.tsx
+++ src/components/MovieSearchResults.tsx
@@ -1,28 +1,40 @@
+import * as css from "./MovieSearchResults.css";
 import { MovieListDisplayParams } from "../LocalTypes/Movie";
 
 export const MovieSearchResultsList = () => (
   <ul>
+    <MovieListItem {...mockMovieTitle()} />
+    <MovieListItem {...mockMovieTitle()} />
+    <MovieListItem {...mockMovieTitle()} />
+    <MovieListItem {...mockMovieTitle()} />
+    <MovieListItem {...mockMovieTitle()} />
     <MovieListItem {...mockMovieTitle()} />
   </ul>
 );
 
 const mockMovieTitle = () => ({
   Title: "Alice",
-  Year: "1990"
+  Year: `${Math.round(Math.random() * 100 + 1900)}`
 });
+
+const Cell = (display: [string, string]) => (
+  <tr>
+    <td>{display[0]}</td>
+    <td>{display[1]}</td>
+  </tr>
+);
 
 export const MovieListItem = (props: MovieListDisplayParams) => {
   const { Title, Year } = props;
   return (
     <li>
-      <div>
-        <h4>Title: </h4>
-        <span>{Title}</span>
-      </div>
-      <div>
-        <h4>Year: </h4>
-        <span>{Year}</span>
-      </div>
+      <img
+        className={css.poster}
+        alt="Poster"
+        src={`http://via.placeholder.com/170x250?text=${Title}-Poster`}
+      />
+      <Cell {...["Title", Title]} />
+      <Cell {...["Year", Year]} />
     </li>
   );
 };
