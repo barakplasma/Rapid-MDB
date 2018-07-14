aa9ac15041b5fdac224b8af57e1e53b08b5cd32b
Michael Salaverry <barakplasma@gmail.com>
2018-07-14 22:12:52 +0300

    feat: movie search component

--- src/components/MovieSearch.tsx
+++ src/components/MovieSearch.tsx
@@ -1,3 +1,11 @@
 import { DebounceInput } from "react-debounce-input";
+import fetch from "isomorphic-unfetch";
 
-export const MovieSearchInput = () => <DebounceInput minLength={3} debounceTimeout={500} onChange={(event) => console.log(`searched for ${event.target.value}`)} />
+import { OMDbMovieSearchResult } from '../LocalTypes/Movie';
+
+export const MovieSearchInput = () => <DebounceInput minLength={3} debounceTimeout={500} onChange={event => console.log(searchForMovies(event.target.value))} />;
+
+const searchForMovies = (title: string): OMDbMovieSearchResult =>
+  fetch(`http://www.omdbapi.com/?s=${title}&apikey=387fe5c2&type=movie`).then(
+    res => res.json()
+  );
